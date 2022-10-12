a974cd92f30c6682249b88246831d855d31928c7
joanna <1059551045@qq.com>
2022-10-12 17:04:13 +0800

    feat: react父子组件传值

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [msg,setMsg] = useState('start');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMsg('end');
+    },1000)
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Child msg={msg} />
       </header>
     </div>
   );
+}
+
+function Child({msg}) {
+  return (
+    <p>{msg}</p>
+  )
 }
 
 export default App;
